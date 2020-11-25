9973c5b7353a795137cea6a0d8196ad2a8b36fa9
Deepanshu Mittal <Deepanshumittal94@gmail.com>
2020-11-25 23:59:59 +0530

    initial commit

---
+++ src/main/java/MyProject/Demo_Deepanshu/DemoDeepanshuApplication.java
@@ -0,0 +1,13 @@
+package MyProject.Demo_Deepanshu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeepanshuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeepanshuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MyProject/Demo_Deepanshu/DemoDeepanshuApplicationTests.java
@@ -0,0 +1,13 @@
+package MyProject.Demo_Deepanshu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeepanshuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
